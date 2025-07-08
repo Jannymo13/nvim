921824809db95fc227226a1ccbbc3f11b239b512
Jannymo13 <jonkonichottil@gmail.com>
2025-07-07 20:47:48 -0400

    added battery and time to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,24 +1,24 @@
-local colors = require('config.colors')
+local colors = require("config.colors")
 
--- local time = require("utility.time")
+local time = require("utility.time")
 
--- local battery = require("utility.battery")
+local battery = require("utility.battery")
 
--- local batteryinfo = {}
--- local function battery_setup()
---     local bString = battery.get_battery()
---     batteryinfo.icon = bString.sub(0, 4)
---     return bString
--- end
--- local function get_battery()
---     vim.go.laststatus = 3
---     return battery_setup()
--- end
+local batteryinfo = {}
+local function battery_setup()
+    local bString = battery.get_battery()
+    batteryinfo.icon = bString.sub(0, 4)
+    return bString
+end
+local function get_battery()
+    vim.go.laststatus = 3
+    return battery_setup()
+end
 
 
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
         options = {
             icons_enabled = true,
@@ -40,41 +40,41 @@ return {
             }
         },
         sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff', 'diagnostics' },
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff", "diagnostics" },
             lualine_c = {
                 {
                     "filename",
                     path = 1
                 },
             },
-            lualine_x = { 'filetype' },
-            lualine_y = { 'location',
-                -- {
-                --     get_battery,
-                --     icon = batteryinfo.icon
-                -- }
+            lualine_x = { "filetype" },
+            lualine_y = { "location",
+                {
+                    get_battery,
+                    icon = batteryinfo.icon
+                }
             },
             lualine_z = {
-            --     {
-            --         time.get_time,
-            --         icon = '', 
-            --         },
+                {
+                    time.get_time,
+                    icon = "", 
+                    },
             }
         },
         inactive_sections = {
             lualine_a = {},
             lualine_b = {},
-            lualine_c = { 'filename' },
-            lualine_x = { 'location' },
+            lualine_c = { "filename" },
+            lualine_x = { "location" },
             lualine_y = { 
-                -- get_battery 
+                get_battery 
             },
             lualine_z = {
-                -- {
-                --     time.get_time,
-                --     icon = '', 
-                -- },
+                {
+                    time.get_time,
+                    icon = "", 
+                },
             }
         },
         tabline = {},
